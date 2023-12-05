067a35131d2b82485030c9fbc6a0e16ca71d5cda
suyashtambe <116366583+suyashtambe@users.noreply.github.com>
2023-12-05 11:04:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,17 +26,24 @@ def takecommand():
             return ""
 
 if __name__ == '__main__':
-    print('PyCharm')
-    say("Hello, I am friday. How may I help you sir?")
+    print('Listening for "Start" command...')
+    say("Hello, I am Friday. Waiting for your command.")
+
+    while True:
+        query = takecommand()
+
+        if "start" in query.lower():
+            say("Hello, how may I assist you, sir?")
+            break
 
     while True:
         print("Listening...")
         query = takecommand()
 
-        sites = [["youtube", "https://www.youtube.com"],
-                 ["google", "https://www.google.com"],
-                 ["wikipedia", "https://www.wikipedia.com"],
-                 ["spotify","https://open.spotify.com/playlist/33VReTZ7AHfx21HOjc4UJJ"]]
+        sites = [["YouTube", "https://www.youtube.com"],
+                 ["Google", "https://www.google.com"],
+                 ["Wikipedia", "https://www.wikipedia.com"],
+                 ["Spotify", "https://open.spotify.com/playlist/33VReTZ7AHfx21HOjc4UJJ"]]
 
         for site in sites:
             if f"Open {site[0]}".lower() in query.lower():
@@ -50,3 +57,17 @@ if __name__ == '__main__':
         if "open notepad" in query.lower():
             say("Opening Notepad sir....")
             open_application("notepad.exe")
+
+        
+        if "how are you" in query.lower():
+            say("I'm doing well, thank you for asking!")
+
+        if "tell me a joke" in query.lower():
+            say("Why did the computer go to therapy? It had too many bytes of emotional baggage!")
+
+        if "who is your creator" in query.lower():
+            say("I was created by OpenAI. They are a team of talented developers!")
+
+        if "Thank you" in query.lower():
+            say("Welcome its my pleasure to help you!")
+  
